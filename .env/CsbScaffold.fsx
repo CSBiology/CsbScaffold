248d3ee000112359363230364cbf1424d143a422
Timo Mühlhaus <Timo Mhlhaus>
2016-08-05 18:29:08 +0200

    add Accord.net via nuget

--- .env/CsbScaffold.fsx
+++ .env/CsbScaffold.fsx
@@ -1,14 +1,26 @@
-  
+// Include MathNet.Numerics
 #r ".lib/MathNet.Numerics.dll"
 #r ".lib/MathNet.Numerics.FSharp.dll"
    
-#r ".lib/FSharpAux.dll"
-#r ".lib/FSharpAux.IO.dll"
-   
+// Include FSharp.Care and IO
+#r ".lib/FSharp.Care.dll"
+#r ".lib/FSharp.Care.IO.dll"
+
+// Include BioFSharp and BioFSharp.Stats   
 #r ".lib/BioFSharp.dll"
 #r ".lib/BioFSharp.IO.dll"
 #r ".lib/BioFSharp.Stats.dll"
 #r ".lib/BioFSharp.Mz.dll"
 
+// Include Accord
+#r "packages/Accord/lib/net45/Accord.dll"
+#r "packages/Accord.Statistics/lib/net45/Accord.Statistics.dll"
+#r "packages/Accord.MachineLearning/lib/net45/Accord.MachineLearning.dll"
+
+
+// Include FSharp.Plotly
 #r "packages/FSharp.Plotly/lib/net40/FSharp.Plotly.dll"
 
+// Include Deedle
+#load "packages/Deedle/Deedle.fsx"
+#load ".aux/DeedleExtensions.fsx"
